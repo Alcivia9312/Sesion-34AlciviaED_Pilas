7ee338c25b0b9a43efa293fd65aaddffd62adcdc
Alcivia9312 <142139263+Alcivia9312@users.noreply.github.com>
2024-10-28 14:36:20 -0600

    confirmacion 1 ok alcivia
    datos de desarrollo: alcivia enrique

---
+++ sesionAlciviaED_PILAS/src/sesionalciviaed_pilas/SesionAlciviaED_PILAS.java
@@ -0,0 +1,22 @@
+package sesionalciviaed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+
+public class SesionAlciviaED_PILAS {
+
+   
+    public static void main(String[] args) {
+        //DECLRACION DE PILA
+        Stack <Integer> pilaAlcivia = new Stack<>();
+        pilaAlcivia.push(100);
+        pilaAlcivia.push(150);
+        pilaAlcivia.push(200);
+        pilaAlcivia.push(250);
+        pilaAlcivia.push(300);
+        
+        JOptionPane.showMessageDialog(null,"datos en pila ordenada es: \n"+pilaAlcivia);
+    }
+    
+}
